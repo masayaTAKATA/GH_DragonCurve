10f529e050161cb86badc30cdc9da0709f7dd4b6
Masaya Takata <takata@meenaxy.com>
2020-04-27 18:19:01 +0900

    Add multi-thread component class( (WIP)

--- DragonCurve/DragonCurveTaskComponent.cs
+++ DragonCurve/DragonCurveTaskComponent.cs
@@ -1,89 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using System.Threading.Tasks;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
 
-/// <summary>
-/// 
-/// </summary>
-
 namespace DragonCurve
 {
-    public class DragonCurveComponent : GH_Component
+    /// <summary>
+    /// Dragon curve(alternative version)
+    /// multi-threading component inherit from GH_TaskCapableComponent
+    /// </summary>
+    public class DragonCurveTaskComponent : GH_TaskCapableComponent<DragonCurveTaskComponent.SolveResults>
     {
-        public DragonCurveComponent() : base("DragonCurve", "DC", "Standard compute DragonCurve","User", "Test")
+        //Constructor
+        public DragonCurveTaskComponent() : base("Dragon curve", "DC_t", "Mult-threading compute Dragon curve", "User", "Test")
         {
         }
-
         #region Input, Output
-        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
             pManager.AddIntegerParameter("Number of depth", "N", "The number of recursive depth level", GH_ParamAccess.item);
             pManager.AddNumberParameter("Length of curves", "L", "The length of curves", GH_ParamAccess.item);
         }
 
-        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
         {
             pManager.AddCurveParameter("Dragon curve", "DragonCrv", "Create dragon curve", GH_ParamAccess.item);
             pManager.AddPointParameter("Curve points", "DragonCrvPts", "Create points of dragon curve", GH_ParamAccess.list);
         }
         #endregion
 
-        /// <summary> Rule Strings
-        /// startString = "FX"
-        ///ruleX = "-FX-Y", "X+YF+"
-        ///ruleY = "-FX-Y"
-        ///
-        ///X : "X+YF+"
-        ///Y : "-FX-Y"
-        ///+ : "Rotate 90"
-        ///- : Rotate -90
-        ///F : Draw Forward
-        /// </summary>
-        protected override void SolveInstance(IGH_DataAccess DA)
+        public class SolveResults
         {
-            //Declare start and rule string(example)
-            string startString = "FX";
-            string ruleX = "X+YF+";
-            string ruleY = "-FX-Y";
+            public Point3d Value { get; set; }
+        }
 
-            int num = 2;
-            double length = double.NaN;
+        /// <summary>
+        /// This is the method that create points list for recursive process
+        /// Create a Compute function that takes the input retrieve from IGH_DataAccess
+        /// and returns an instance of SolveResults
+        /// </summary>
+        private static SolveResults ComputeDragonPts(double length, int num)
+        {
+            var result = new SolveResults();
+
             
-            if(!DA.GetData(0, ref num)) { return; }
-            if(!DA.GetData(1, ref length)) { return; }
-
-            //we should now validate the data and warn the user if invalid data is supplied.
-            if(num <= 1)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Number of depth must be bigger than One.");
-                return;
-            }
-            if(length < 0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Length must be bigger than Zero.");
-                return;
-            }
-            if(num > 10)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Number of depth is recommended under Ten");
-                return;
-            }
-
-            //Declare a string
-            var dragonString = startString;
-            //Generate the string.
-            GrowString(ref num, ref dragonString, ruleX, ruleY);
-            //Generate the points
-            var dragonPts = new List<Point3d>();
-            ParceDragonString(dragonString, length, ref dragonPts);
-            //Create the curve
-            var dragonCrv = new PolylineCurve(dragonPts);
-
-            //Assign output
-            DA.SetData(0, dragonCrv);
-            DA.SetDataList(1, dragonPts);
+            return result;
         }
 
         //Generate GrowString from rules
@@ -95,18 +58,18 @@ namespace DragonCurve
 
             //Create new string
             string newString = "";
-            for(int i = 0; i < finalString.Length; i++)
+            for (int i = 0; i < finalString.Length; i++)
             {
                 rule = finalString[i];
-                if(rule == 'X')
+                if (rule == 'X')
                 {
                     newString = newString + ruleX;
                 }
-                if(rule == 'Y')
+                if (rule == 'Y')
                 {
                     newString = newString + ruleY;
                 }
-                if(rule == 'F' | rule == '+' | rule == '-')
+                if (rule == 'F' | rule == '+' | rule == '-')
                 {
                     newString = newString + rule;
                 }
@@ -121,10 +84,8 @@ namespace DragonCurve
 
             //Grow again(recursive)
             GrowString(ref num, ref finalString, ruleX, ruleY);
-
         }
 
-        //Dragon curves is generate from the dragonString
         private void ParceDragonString(string dragonString, double length, ref List<Point3d> dragonPoints)
         {
             //parce instruction string to generate points
@@ -133,7 +94,7 @@ namespace DragonCurve
             dragonPoints.Add(pt);
 
             //drawing direction vector - start along the x-axis
-            //vector direction will be rotated depending on (+, -) instruction.
+            //vector direction will be rotated depending on (+, -)instruction.
             var vec = new Vector3d(1.0, 0.0, 0.0);
 
             char rule;
@@ -152,7 +113,7 @@ namespace DragonCurve
                 {
                     vec.Rotate(Math.PI / 2, Vector3d.ZAxis);
                 }
-                //rotate Right.
+                //rotate Right
                 if(rule == '-')
                 {
                     vec.Rotate(-Math.PI / 2, Vector3d.ZAxis);
@@ -160,11 +121,44 @@ namespace DragonCurve
             }
         }
 
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Declare placeholder valuable for the input data
+            double length = double.NaN;
+            int num = 2;
+            
+            //Retrieve input data
+            if(!DA.GetData(0, ref num)) { return; }
+            if(!DA.GetData(1, ref length)) { return; }
+
+            if (InPreSolve)
+            {
+                //Queue up the task
+                Task<SolveResults> task = Task.Run(() => ComputeDragonPts(length, num));
+                TaskList.Add(task);
+
+                return;
+            }
+
+            if(!GetSolveResults(DA, out SolveResults result))
+            {
+                //Compute results on a given data
+                result = ComputeDragonPts(length, num);
+            }
+
+            //Set out put data
+            if(result != null)
+            {
+                DA.SetData(0, new PolylineCurve(result.Value));
+                DA.SetDataList(1, result.Value);
+            }
+            
+        }
 
         protected override System.Drawing.Bitmap Icon => null;
 
         public override GH_Exposure Exposure => GH_Exposure.primary | GH_Exposure.obscure;
 
-        public override Guid ComponentGuid => new Guid("{3ACABAC8-632C-4CB5-ABDF-EC3BE35BEC0B}");
+        public override Guid ComponentGuid => new Guid("{68269F36-29E2-45F1-8A7E-707CF60BBF37}");
     }
 }
